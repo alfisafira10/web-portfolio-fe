e0619af0515c426d0a9ca0cf75896819fe4cbb82
alfisafira10 <alfisafira69@gmail.com>
2023-06-03 23:44:08 +0700

    First Commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,45 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+</script>
+
+<template>
+  <h1>About Me</h1>
+  <p>Hello, my name is Alfi. I'm from Gresik, Indonesia. </p>
+  <p>I'm a student majoring in Information Systems. Interested and motivated in seeking 
+    opportunities to gain experience in technology, such as UI/UX Design, Web Development,
+    and Machine Learning. Also always want to try new experiences, new opportunities, and 
+    learn to improve myself for a professional career.</p>
+  <br />
+  <h1>Project List</h1>
+  <p>Below are some of my projects : </p>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Basic Portfolio</template>
+
+    Basic portfolio from Basic Web Programming course.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Our Tailor</template>
+
+    A simple website with case studies in the 
+    fictional convection industry, namely Our Tailor.
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <EcosystemIcon />
+    </template>
+    <template #heading>Buahin</template>
+
+    Fruit type detection app from my team's final project on MSIB Batch 3.
+  </WelcomeItem>
+</template>
---
+++ src/views/ContactView.vue
@@ -0,0 +1,48 @@
+<script>
+import BaseInput from '@/components/BaseInput.vue';
+
+export default {
+    name: 'ContactView',
+    components: {
+        BaseInput,
+    },
+    data: () => ({
+        title: 'Contact',
+        // input
+        input: {
+            name: '',
+            email: '',
+            message: '',
+        }
+    }),
+    methods: {
+        submit() {
+            console.log(this.input);
+        }
+    },
+};
+</script>
+
+<template>
+    <div class="contact">
+        <h1>{{ title }}</h1>
+        <form Asubmit.prevent="submit">
+            <BaseInput v-model="input.name" name="name" label="Name" type="text" />
+            <BaseInput v-model="input.email" name="email" label="Email" type="email" />
+            <br />
+            <span>Message :</span>
+            <p style="white-space: pre-line">{{ message }}</p>
+            <textarea v-model="message" placeholder="Your message..."> </textarea>
+            <br />
+            <button type="submit">Submit</button>
+        </form>
+    </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+    .contact {
+        min-height: 100vh;
+    }
+}
+</style>
